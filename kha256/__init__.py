633471b71b6c845f1188f632c5c259f9ebc24a49
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-01-06 01:22:27 +0300

    Update __init__.py

--- kha256/__init__.py
+++ kha256/__init__.py
@@ -21,7 +21,7 @@ import warnings
     # importlib.reload(kha256) # F821 undefined name 'kha256'
 
 # Paket sürüm numarası
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "Mehmet Keçeci"
 __license__ = "AGPL-3.0"
 __copyright__ = "Copyright 2025 Mehmet Keçeci"
