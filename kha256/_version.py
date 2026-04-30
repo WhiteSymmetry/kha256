2579e3c884d45b3c6b8fe51489694d978f7195a2
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-04-30 12:36:49 +0000

    Bump version from 0.2.5 to 0.2.6

--- kha256/_version.py
+++ kha256/_version.py
@@ -11,7 +11,7 @@ from typing import Dict, List, Tuple
 # - MAJOR: Incompatible API changes
 # - MINOR: Backwards-compatible functionality additions
 # - PATCH: Backwards-compatible bug fixes
-__version__: str = "0.2.5"
+__version__: str = "0.2.6"
 
 # License information
 __license__: str = "AGPL-3.0-or-later"
