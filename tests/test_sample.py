535862029302fcdf792dd51b0b1ed0cefecb0096
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-06-11 20:22:54 +0300

    Add salt to KHA-256 hash generation in tests
    Updated the hash function to include a salt for enhanced security.

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -6,8 +6,13 @@ Tests core functionality, number type generation, and mathematical properties.
 """
 
 import kha256
+import secrets
+import os
+
+salt = os.urandom(16)
+#salt = secrets.token_bytes(64)
 
 # Basit test
 hasher = kha256.generate_fortified_hasher()
-hash_result = hasher.hash("Merhaba Dünya!")
+hash_result = hasher.hash("Merhaba Dünya!", salt)
 print(f"KHA-256 Hash: {hash_result}")
