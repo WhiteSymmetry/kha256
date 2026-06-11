6bb65d4002c3e1970754ee305f38c884c960b628
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-06-11 21:01:56 +0300

    Add tests for fortified hasher in test_sample.py
    Added tests for fortified hasher functionality including output generation, determinism, and input variation.

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -9,6 +9,36 @@ import kha256
 import secrets
 import os
 
+def test_fortified_hash_generates_output():
+    """Test that fortified hasher produces hash output"""
+    salt = os.urandom(16)
+    hasher = kha256.generate_fortified_hasher()
+    hash_result = hasher.hash("Merhaba Dünya!", salt)
+    
+    assert hash_result is not None
+    assert len(hash_result) > 0
+    print(f"KHA-256 Hash: {hash_result}")
+
+def test_hash_is_deterministic():
+    """Test that same input produces same output"""
+    salt = os.urandom(16)
+    hasher = kha256.generate_fortified_hasher()
+    
+    result1 = hasher.hash("test", salt)
+    result2 = hasher.hash("test", salt)
+    
+    assert result1 == result2
+
+def test_hash_different_inputs_produce_different_outputs():
+    """Test that different inputs produce different hashes"""
+    salt = os.urandom(16)
+    hasher = kha256.generate_fortified_hasher()
+    
+    result1 = hasher.hash("input1", salt)
+    result2 = hasher.hash("input2", salt)
+    
+    assert result1 != result2
+
 salt = os.urandom(16)
 #salt = secrets.token_bytes(64)
 
