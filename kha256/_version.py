e047f404188d844d847d7433b210a6cf9f1a8cfa
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-06-26 19:16:02 +0300

    Bump version from 0.4.0 to 0.4.1

--- kha256/_version.py
+++ kha256/_version.py
@@ -14,7 +14,7 @@ from typing import Dict, List, Tuple, Optional
 # - MAJOR: Incompatible API changes
 # - MINOR: Backwards-compatible functionality additions
 # - PATCH: Backwards-compatible bug fixes
-__version__: str = "0.4.0"
+__version__: str = "0.4.1"
 
 # License information
 __license__: str = "AGPL-3.0-or-later"
